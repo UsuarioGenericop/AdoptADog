630187ce14e8d9f3d7a46bb7d65e6d78f29e86d5
Andres <andydquintero@gmail.com>
2025-09-09 21:52:46 -0500

    TestinReact'sCode

--- AdoptADog/src/components/producto.jsx
+++ AdoptADog/src/components/producto.jsx
@@ -1,4 +1,15 @@
+import React from "react";
 const Producto = () => {
-  return <h1> Producto </h1>;
+  const productName = "Producto 1";
+  return (
+    <>
+      <h1> {productName} </h1>
+      <p>Operaciones matemática Suma: {2 + 2}</p>
+      <p>Operaciones matemática Resta: {2 - 2}</p>
+      <p>Operaciones matemática Multiplicación: {2 * 2}</p>
+      <p>Operaciones matemática División: {2 / 2}</p>
+      <p>Operaciones matemática Módulo: {2 % 2}</p>
+    </>
+  );
 };
 export default Producto;
