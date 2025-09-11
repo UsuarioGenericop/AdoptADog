a4bca2ca4f265f540398061f23b611b129bd8c37
Andres <andydquintero@gmail.com>
2025-09-10 23:17:58 -0500

    ReactStyles

--- AdoptADog/src/components/producto.jsx
+++ AdoptADog/src/components/producto.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 const Producto = () => {
   const productName = "Producto 1";
+  const myStyle = { fontSize: "12px", color: "brown" };
   return (
-    <>
-      <h1> {productName} </h1>
-      <p>Operaciones matemática Suma: {2 + 2}</p>
-      <p>Operaciones matemática Resta: {2 - 2}</p>
+    <div>
+      <h1 className="producto"> {productName} </h1>
+      <p style={{ fontSize: "12px", color: "blue" }}>
+        Operaciones matemática Suma: {2 + 2}
+      </p>
+      <p style={myStyle}>Operaciones matemática Resta: {2 - 2}</p>
       <p>Operaciones matemática Multiplicación: {2 * 2}</p>
       <p>Operaciones matemática División: {2 / 2}</p>
       <p>Operaciones matemática Módulo: {2 % 2}</p>
-    </>
+    </div>
   );
 };
 export default Producto;
